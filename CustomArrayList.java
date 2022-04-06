b035e3b1a50d4f9af7d042850ea664054c7186b5
tristandanle <letristandan@gmail.com>
2022-04-06 16:38:25 -0400

    Update CustomArrayList.java

--- CustomArrayList.java
+++ CustomArrayList.java
@@ -6,7 +6,7 @@ public class CustomArrayList<T> implements CustomList<T> {
 	private static final int MIN_CAPACITY = 10;
 	Object[] items = new Object[MIN_CAPACITY];
 	private boolean added = false;
-	private int i = 0;
+	private int i = 0; // use to trace of the current size in array - increase size when an item is added
 	private int current_Capacity = 0;
 	private int newSize = 0;
 	private int oldSize = 0;
@@ -34,7 +34,7 @@ public class CustomArrayList<T> implements CustomList<T> {
 	@Override
 	public int getSize() {
 		// System.out.println(items.length);
-		return items.length;
+		return i; // current size of array
 	}
 
 	@Override
