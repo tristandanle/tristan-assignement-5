e2496faee943646748adfb8ba320062bd3a59c79
tristandanle <letristandan@gmail.com>
2022-04-10 21:35:07 -0400

    Update CustomArrayList.java

--- CustomArrayList.java
+++ CustomArrayList.java
@@ -5,36 +5,30 @@ import java.util.Arrays;
 public class CustomArrayList<T> implements CustomList<T> {
 	private static final int MIN_CAPACITY = 10;
 	Object[] items = new Object[MIN_CAPACITY];
-	private boolean added = false;
-	private int i = 0; // use to trace of the current size in array - increase size when an item is added
-	private int current_Capacity = 0;
-	private int newSize = 0;
-	private int oldSize = 0;
+
+	private boolean added = true; // true if an element is added successfully 
+	private int i = 0; // size of array is increased when adding item
 
 	@Override
 	public boolean add(T item) {
-		++current_Capacity;
-		if (getSize() >= current_Capacity) {
-			items[i] = item;
-			i++;
-
-		} else if (getSize() <= current_Capacity) {
-			oldSize = items.length;
-			newSize = oldSize + 20; // expand the array every 20 items reached the max
-			// System.out.println("array overload - over capacity");
-			items = Arrays.copyOf(items, newSize);
-			items[i] = item;
-			i++;
-		} else {
-			return false;
+	
+		items[i++] = item;
+		if (i == items.length) {
+			Object[] copyArray = items;
+			items = Arrays.copyOf(copyArray,copyArray.length + 20);
+			for (int c = 0; c < copyArray.length; c++) {
+				items[c] = copyArray[c];
+			};
+			added = true;
+		}else {
+		 added = false;
 		}
-		return added;
+       return added;
 	}
 
 	@Override
 	public int getSize() {
-		// System.out.println(items.length);
-		return i; // current size of array
+		return i;
 	}
 
 	@Override
