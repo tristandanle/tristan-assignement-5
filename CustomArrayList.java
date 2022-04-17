189e6fc6e55e2efd1f8b997c63306d1fe542c608
tristandanle <letristandan@gmail.com>
2022-04-17 18:54:43 -0400

    Update CustomArrayList.java
    Latest modification: 2022-04-17 
    Implement algorithms, coding implementation of the ArrayList data structure
    How to grow the backing Object array in double size when adding a new element and the backing Object array is full.

--- CustomArrayList.java
+++ CustomArrayList.java
@@ -1,34 +1,24 @@
 package com.coderscampus.arraylist;
 
-import java.util.Arrays;
-
 public class CustomArrayList<T> implements CustomList<T> {
-	private static final int MIN_CAPACITY = 10;
-	Object[] items = new Object[MIN_CAPACITY];
-
-	private boolean added = true; // true if an element is added successfully 
-	private int i = 0; // size of array is increased when adding item
+	final static int CAPACITY = 10;
+	Object[] items = new Object[CAPACITY];
+	private int cc = 0; // current capacity/size of array
 
 	@Override
 	public boolean add(T item) {
-	
-		items[i++] = item;
-		if (i == items.length) {
-			Object[] copyArray = items;
-			items = Arrays.copyOf(copyArray,copyArray.length + 20);
-			for (int c = 0; c < copyArray.length; c++) {
-				items[c] = copyArray[c];
-			};
-			added = true;
-		}else {
-		 added = false;
+		if (items.length == cc) {
+			Object[] newArray = new Object[items.length * 2];
+			System.arraycopy(items, 0, newArray, 0, items.length);
+			items = newArray;
 		}
-       return added;
+		items[cc++] = item;
+		return true;
 	}
 
 	@Override
 	public int getSize() {
-		return i;
+		return cc;
 	}
 
 	@Override
